1c6dee2d99bf4a80583a9fdbfbb3376fc94aa62d
jeremie-gerbenne <81633769+jeremie-gerbenne@users.noreply.github.com>
2021-04-12 18:39:11 +0200

    td sur la creation de class sur python

---
+++ OOP/td2_class.py
@@ -0,0 +1,45 @@
+class FastFood:
+    def _init_(self,price,size,orderNumber):
+        self.price = price
+        self.size = size
+        self.orderNumber = orderNumber
+
+    def set_size(self,size):
+        self.size = size
+
+    def get_price(self):
+        return self.price
+
+    def get_size(self):
+        return self.size
+
+    def get_orderNumber(self):
+        return self.orderNumber
+
+    def print_orderNumber(self):
+        print(self.orderNumber)
+
+class Tacos(FastFood):
+    def tacosAttributes(self, meat, sauce):
+        self.meat = meat
+        self.sauce = sauce
+
+    def __str__(self):
+        meats = ""
+        for meat_list in self.meat:
+            meats += meat +" "
+        return "It is the tacos of order number " + str(get_orderNumber()) + "with : " + meats + "and : " + self.sauce
+
+    def get_ingredient(self)
+        return [ self.meat[i] for i in range(len(self.meat)), self.sauce[j] for j in range(len(self.sauce))]
+
+class Burger(FastFood):
+    def burgerAttributes(self,name,sauce)
+        self.name = name
+        self.sauce = sauce
+
+    def print_name(self):
+        print(self.name)
+
+    def get_sauce(self):
+        return self.sauce
